98697a3e0522e876aa07030caedf957a447d4748
Seong Hyeri <gofl0721@withere.com>
2024-02-06 18:58:33 +0900

    ~ #4.4 5:25 이어서

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
 
-function onLoginSubmit(event){ //argument event로 브라우저 info받아옴
-	event.preventDefault(); //어떤 event의 기본행동이 발생되지 않도록 막는 함수
-	// const userName = loginInput.value;
-	console.log(loginInput.value);
+function onLoginSubmit(event){
+	event.preventDefault(); //기본동작 막기
+	loginForm.classList.add("hidden"); //form 숨기기
+	const username = loginInput.value; //유저이름 변수로 저장 
+	greeting.innerText = "Hello " + username; //저장한 이름 h1안에 넣어줌
+	greeting.classList.remove("hidden");
+}
+function handleLinkClick(event){
+	event.preventDefault();
+	console.log(event);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-// submit은 엔터를 누르거나 버튼을 클릭할때 발생
-// 새로고침이 일어나는건 form submit의 기본 동작, 브라우저의 프로그래밍
-
-//브라우저가 이벤트 발생시 함수 호출 
-//브라우저내에서 event로부터 정보를 잡아내서(argument를 가지고) 실행버튼을 누름
-
-//모든 EventListener function의 첫번째 argument는 항상 지금 막 벌어진 일에 대한 정보임
-//argument 공간만 주면 JS가 알아서 방금 일어난 event에 대한 정보를 채워넣어줌
-
-//기본행동: 어떤 funciton에 대해 브라우저가 기본적으로 수행하는 동작
-// 여기서의 기본행동 => 누군가 submit하면 브라우저는 기본적으로 페이지 새로고침함 
-//preventDefault() 로 막음
-//이 함수는 EventListener함수의 첫번째 argument안에 있는 fucntion임
-
-//submit이벤트 발생할 때 JS는 onLoginSubmit 함수 호출하고 
-//여기에 event argument에 정보를 넣어주고 실행(기본동작막아줌)
+link.addEventListener("click", handleLinkClick);
