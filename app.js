7e52cc40160a00b5f4ffb94747ad65f01e5bfe52
Seong Hyeri <gofl0721@withere.com>
2024-02-08 14:43:54 +0900

    #4.5 //

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
-const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden"; //string만 포함된 변수는 대문자로 표기함(string저장)
 function onLoginSubmit(event){
-	event.preventDefault(); //기본동작 막기
-	loginForm.classList.add("hidden"); //form 숨기기
-	const username = loginInput.value; //유저이름 변수로 저장 
-	greeting.innerText = "Hello " + username; //저장한 이름 h1안에 넣어줌
-	greeting.classList.remove("hidden");
-}
-function handleLinkClick(event){
-	event.preventDefault();
-	console.log(event);
+	event.preventDefault(); 
+	loginForm.classList.add(HIDDEN_CLASSNAME);
+	const username = loginInput.value;
+	localStorage.setItem("username", username);
+	greeting.innerText = `Hello ${username}` 
+	greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+
+//새로고침 하면 다시 form이 나타나니까 
+//localStorage에 username있으면 form 안보이고 h1 보이도록,
+//유저정보 없다면 form이 먼저 표시되도록 만들어줄것임
